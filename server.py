a2b85f2e38eeb414574bd67cbea43a5849f98003
Julie Coleman <juliecoleman820@gmail.com>
2020-05-17 00:16:45 +0000

    Added event search api

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, requestsour
 
 from pprint import pformat
 import os
@@ -41,7 +41,12 @@ def find_afterparties():
     sort = request.args.get('sort', '')
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
-    payload = {'apikey': API_KEY}
+    payload = {'apikey': API_KEY,
+               'keyword': keyword,
+               'postalCode': postalcode,
+               'radius': radius,
+               'unit': unit,
+               'sort': sort}
 
     # TODO: Make a request to the Event Search endpoint to search for events
     #
@@ -54,9 +59,11 @@ def find_afterparties():
     # - Replace the empty list in `events` with the list of events from your
     #   search results
 
-    data = {'Test': ['This is just some test data'],
-            'page': {'totalElements': 1}}
-    events = []
+    res = requests.get(url, params=payload)
+
+    data = res.json()
+
+    events = data['_embedded']['events']
 
     return render_template('search-results.html',
                            pformat=pformat,
